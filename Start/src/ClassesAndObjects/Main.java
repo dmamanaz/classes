1852bb3b361267eaf759a4655395567395d476af
Doni Mamanazarov <doniyorad@gmail.com>
2020-01-02 19:06:10 -0600

    wrapping up classes

--- Start/src/ClassesAndObjects/Main.java
+++ Start/src/ClassesAndObjects/Main.java
@@ -3,26 +3,40 @@ package ClassesAndObjects;
 public class Main {
     public static void main(String[] args) {
 
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Life");
+        stringBuilder.append(" is great");
+        stringBuilder.append(" when you are happy!");
+
+
+        stringBuilder.delete(1, 2);
+
+        System.out.println(stringBuilder);
+        System.out.println(stringBuilder.capacity());
+
+
         // Ball ball = new Ball("blue", "justBall", 0, 12);
-        Ball ball = new Ball();
-
-        Baseball baseball = new Baseball();
-        baseball.color = "yellow";
-        baseball.capacity = 23;
-        baseball.name = "Baseball";
-        baseball.shape = "round";
-        baseball.size = 2;
-
-
-        baseball.bounce();
-
-
-
-        System.out.println(baseball.getCapacity());
-        System.out.println(baseball.getColor());
-        System.out.println(baseball.getName());
-        System.out.println(baseball.shape);
-        System.out.println(baseball.size);
+//        Ball ball = new Ball();
+//
+//        Baseball baseball = new Baseball();
+//        baseball.color = "yellow";
+//        baseball.capacity = 23;
+//        baseball.name = "Baseball";
+//        baseball.shape = "round";
+//        baseball.size = 2;
+//
+//
+//        baseball.bounce();
+//
+//
+//
+//        System.out.println(baseball.getCapacity());
+//        System.out.println(baseball.getColor());
+//        System.out.println(baseball.getName());
+//        System.out.println(baseball.shape);
+//        System.out.println(baseball.size);
 
 
 
